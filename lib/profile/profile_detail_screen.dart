1c58526e076cab76933fa64c25009b3bff4b4aa5
Bayu Arma Praja <100572149+Yuu18id@users.noreply.github.com>
2023-03-15 16:32:48 +0700

    Update profile_detail_screen.dart

--- lib/profile/profile_detail_screen.dart
+++ lib/profile/profile_detail_screen.dart
@@ -9,28 +9,149 @@ class ProfileDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('latihan'),
+        title: const Text('211112007 - Bayu Arma Praja'),
       ),
-
-      body: Row(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            
-            children: const [
-              Center(
-                
-                child: Text('Strawberry Pavlona',
-                style: TextStyle(
-                  color: Colors.green,
-                ),),
+      body: Center(
+        child: Column(
+          children: [
+            const Text('Ini adalah detail Profile'),
+ElevatedButton(
+onPressed: () {
+Navigator.pop(context);
+},
+child: const Text('Kembali')),
+            Container(
+              width: 910,
+              child: Row(
+                children: [
+                  Container(
+                    height: 500,
+                    child: Center(
+                      child: Container(
+                        width: 300,
+                        height: 380,
+                        padding: EdgeInsets.all(13),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                                padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                color: Colors.black,
+                                width: 1,
+                              ),color: Colors.blue[50],),
+                              child: const Center(
+                                child: Text(
+                                  'Strawberry Pavlova',
+                                  style:
+                                      TextStyle(fontFamily: 'Roboto', fontSize: 20),
+                                ),
+                              ),
+                            ),
+                            Container(
+                                padding: const EdgeInsets.all(5),
+                              width: 350,
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                color: Colors.black,
+                                width: 1,
+                                
+                              ),
+                              color: Colors.blue[50],),
+                              
+                              
+                              child: const Center(
+                                child: Text(
+                                  'Pavlova is a meringue-based dessert named after the Russian ballerine Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.',
+                                  style:
+                                      TextStyle(fontFamily: 'Roboto', fontSize: 18),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            Container(
+                                padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                color: Colors.black,
+                                width: 1,
+                              ), color: Colors.blue[50],),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Row(
+                                    children: const [
+                                      Icon(Icons.star, color: Colors.black),
+                                      Icon(Icons.star, color: Colors.black),
+                                      Icon(Icons.star, color: Colors.black),
+                                      Icon(Icons.star, color: Colors.black),
+                                      Icon(Icons.star, color: Colors.black),
+                                    ],
+                                  ),
+                                  const Text(
+                                    '170 Reviews',
+                                    style: TextStyle(
+                                      fontFamily: 'Roboto',
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                color: Colors.black,
+                                width: 1,
+                              ), color: Colors.blue[50],),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Column(
+                                    children: [
+                                      const Icon(Icons.kitchen, color: Colors.green),
+                                      const Text('PREP:'),
+                                      Container(
+                                        child: const Text('25 min'),
+                                        padding: const EdgeInsets.only(top: 15),
+                                      )
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      const Icon(Icons.timer, color: Colors.green),
+                                      const Text('COOK:'),
+                                      Container(
+                                          child: const Text('1 hr'),
+                                          padding: const EdgeInsets.only(top: 15))
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      const Icon(Icons.restaurant, color: Colors.green),
+                                      const Text('FEEDS:'),
+                                      Container(
+                                          child: const Text('4-6'),
+                                          padding: const EdgeInsets.only(top: 15))
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Image.asset(
+                    'Pavlova.jpg',
+                  )
+                ],
               ),
-              Text('Pavlona is a meringue-based dessert named after the Russian ballerine Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.'),
-            ],
-          ),
-          Image.asset('assets/mario.png', width: 400,)
-        ],
-        
+            ),
+          ],
+        ),
       ),
     );
   }
