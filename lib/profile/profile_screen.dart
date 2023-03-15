da9ee5f886eea193bb65cc1b3361d0bbbafa403d
Bayu Arma Praja <100572149+Yuu18id@users.noreply.github.com>
2023-03-15 16:32:26 +0700

    Update profile_screen.dart

--- lib/profile/profile_screen.dart
+++ lib/profile/profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_application_1/profile/profile_detail_screen.dart';
+import 'package:tugas/profile/profile_detail_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -9,19 +9,18 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
- title: const Text('Profile'),
- ),
- body: Center(
- child: ElevatedButton(
- onPressed: () {
- Navigator.push(
- context,
- MaterialPageRoute(
- builder: (context) => const ProfileDetailScreen()));
- },
- child: const Text('Profile Detail'),
- )
-    ));
+        appBar: AppBar(
+          title: const Text('211112007 - Bayu Arma Praja'),
+        ),
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ProfileDetailScreen()));
+          },
+          child: const Text('Profile Detail'),
+        )));
   }
 }
